bf0728da46bd9d8f9ee181363d44db3352528a89
Nigel <nigel.ong00@gmail.com>
2022-11-08 11:57:12 -0800

    created the objects and const array

---
+++ Script/script.js
@@ -0,0 +1,16 @@
+let course1 = {
+    code:"ACIT 1515", 
+    course:"Scripting for IT",
+} 
+let course2 = {
+    code:"ACIT 1620", 
+    course:"Fundamental Web Technologies",
+}
+let course3 = {
+    code:"ACIT 1630", 
+    course:"Database Systems",
+}
+
+const courseList = [course1, course2, course3
+
+]
